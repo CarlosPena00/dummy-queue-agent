e955fc967e92b95767ab7ee2f37cdd6766ef248f
Carlos Pena <chcp@cin.ufpe.br>
2025-06-15 15:05:09 -0300

    fixes

--- src/core/settings.py
+++ src/core/settings.py
@@ -43,8 +43,6 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
-        env_prefix="INGEST_",
-        case_sensitive=False,
     )
 
     def get_rabbitmq_url(self) -> str:
