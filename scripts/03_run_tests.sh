db6a5d37d0aac5dde969544ec6964b0b99f66f3b
Carlos Pena <chcp@cin.ufpe.br>
2025-06-16 11:09:06 -0300

    chore: Update coverage settings and requirements files

--- scripts/03_run_tests.sh
+++ scripts/03_run_tests.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-# Run tests with pytest
+# Run tests with pytest and coverage
 # Optional arguments can be passed to pytest
 
-# Run all tests by default
+# Clean previous coverage data
+coverage erase
+
+# Run all tests by default with coverage
 if [ $# -eq 0 ]; then
-    python -m pytest tests/
+    coverage run -m pytest tests/
 else
     # Pass all arguments to pytest
-    python -m pytest "$@"
+    coverage run -m pytest "$@"
 fi
+
+# Generate coverage report
+coverage report -m
+# Generate HTML report
+coverage html
